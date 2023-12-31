773fce4e86f7e77f86b22e6d25213568d94af235
SooHwanC <frcp9408@gmail.com>
2023-12-31 14:54:38 +0900

    12.31_10951

---
+++ src/jun10951.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class jun10951 {
+
+    public static void main(String[] args) throws Exception {
+
+        Scanner scan = new Scanner(System.in);
+
+        while (scan.hasNext()) {
+
+            int num1 = scan.nextInt();
+            int num2 = scan.nextInt();
+
+            System.out.println(num1 + num2);
+        }
+
+    }
+
+}
