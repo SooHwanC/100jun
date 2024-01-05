1f1d30ad8a3b1abc403745b9c9435b0b0544e374
SooHwanC <frcp9408@gmail.com>
2024-01-05 16:59:13 +0900

    0105_5597

---
+++ src/baeyul1cha/jun5597.java
@@ -0,0 +1,24 @@
+package baeyul1cha;
+
+import java.util.Scanner;
+
+public class jun5597 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int arr[] = new int[30];
+
+        for (int i = 0; i < 28; i++) {
+            arr[scan.nextInt() - 1] = 1;
+
+        }
+        for (int i = 0; i < 30; i++) {
+            if (arr[i] == 0) {
+                System.out.println(i + 1);
+            }
+        }
+
+    }
+
+}
